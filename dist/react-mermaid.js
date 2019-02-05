30934d5a0ba924a436eef1e5232165b7cf376e99
David ML Brown Jr <david.brown@pnnl.gov>
2019-02-05 11:52:51 -0800

    Major version bump to include new mermaid and updated node support
    Signed-off-by: David ML Brown Jr <david.brown@pnnl.gov>

--- dist/react-mermaid.js
+++ dist/react-mermaid.js
@@ -4,47 +4,66 @@ Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
 var _mermaid = require('mermaid');
 
 var _mermaid2 = _interopRequireDefault(_mermaid);
 
-var Mermaid = _react2['default'].createClass({
-  displayName: 'Mermaid',
+var Mermaid = (function (_Component) {
+  _inherits(Mermaid, _Component);
 
-  propTypes: {
-    name: _react2['default'].PropTypes.string
-  },
+  function Mermaid(props) {
+    _classCallCheck(this, Mermaid);
 
-  getDefaultProps: function getDefaultProps() {
-    return {
-      name: 'mermaid'
-    };
-  },
-
-  getInitialState: function getInitialState() {
-    return {
+    _get(Object.getPrototypeOf(Mermaid.prototype), 'constructor', this).call(this, props);
+    this.state = {
       diagram: 'Loading diagram...'
     };
-  },
-
-  componentDidMount: function componentDidMount() {
-    var _this = this;
-
-    _mermaid.mermaidAPI.render(this.props.name, this.props.children.toString(), function (html) {
-      return _this.setState({ diagram: html });
-    });
-  },
-
-  render: function render() {
-    return _react2['default'].createElement('div', { className: 'mermaid', dangerouslySetInnerHTML: { __html: this.state.diagram } });
   }
-});
+
+  _createClass(Mermaid, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      var _this = this;
+
+      _mermaid.mermaidAPI.render(this.props.name, this.props.children.toString(), function (html) {
+        return _this.setState({ diagram: html });
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return _react2['default'].createElement('div', { className: 'mermaid', dangerouslySetInnerHTML: { __html: this.state.diagram } });
+    }
+  }]);
+
+  return Mermaid;
+})(_react.Component);
+
+Mermaid.propTypes = {
+  name: _propTypes2['default'].string
+};
+
+Mermaid.defaultProps = {
+  name: 'mermaid'
+};
 
 exports['default'] = Mermaid;
 module.exports = exports['default'];
--- src/react-mermaid.js
+++ src/react-mermaid.js
@@ -1,33 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import mermaid, {mermaidAPI} from 'mermaid';
 
-const Mermaid = React.createClass({
+class Mermaid extends Component {
 
-  propTypes: {
-    name: React.PropTypes.string
-  },
-
-  getDefaultProps () {
-    return {
-      name: 'mermaid'
-    };
-  },
-
-  getInitialState () {
-    return {
+  constructor(props) {
+    super(props);
+    this.state = {
       diagram: 'Loading diagram...'
     };
-  },
+  }
 
   componentDidMount () {
     mermaidAPI.render(this.props.name, this.props.children.toString(), (html) => this.setState({diagram: html}));
-  },
+  }
 
   render () {
     return (
       <div className="mermaid" dangerouslySetInnerHTML={{__html: this.state.diagram}}></div>
     )
   }
-});
+}
+
+Mermaid.propTypes = {
+  name: PropTypes.string
+};
+
+Mermaid.defaultProps = {
+  name: 'mermaid'
+};
 
 export default Mermaid;
